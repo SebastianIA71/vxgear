b5a8f1b7f66c69f53317d04b53ef2806e8e45050
SebastianIA71 <sebastianfont71@gmail.com>
2025-11-17 21:13:04 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import vercelAnalytics from "@vercel/analytics/astro";
 
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [
+    vercelAnalytics()
     tailwind({
       applyBaseStyles: true,
     }),
